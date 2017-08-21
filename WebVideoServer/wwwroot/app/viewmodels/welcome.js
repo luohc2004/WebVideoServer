ff37914c07ed7437246a32c6baee7d415f57a744
FrenzyBoy <luohc2004@163.com>
2017-08-22 00:09:06 +0800

    测试了分页功能

--- WebVideoServer/wwwroot/app/viewmodels/welcome.js
+++ WebVideoServer/wwwroot/app/viewmodels/welcome.js
@@ -1,15 +1,14 @@
 ﻿define(['durandal/app', 'knockout'], function (app, ko) {
     var ctor = function () {
         this.displayName = '天天学舞蹈的视频';
+        var data = [];
+        for (var i = 1; i < 23; i++) {
+            data.push({
+                _id: i, title: '视频' + i, uploadDate: (new Date()).toLocaleDateString(), captureDate: (new Date()).toLocaleDateString()
+            });
+        }
         this.gridConfig = {
-            data: [{
-                _id: 1, title: '视频1', uploadDate: (new Date()).toLocaleDateString(), captureDate: (new Date()).toLocaleDateString(), onClick: function () {
-                    alert(this.title);
-
-                }
-            },
-            { _id: 2, title: '视频2', uploadDate: (new Date()).toLocaleDateString(), captureDate: (new Date()).toLocaleDateString() },
-            ]
+            data: data
         };
     }
     return ctor;
