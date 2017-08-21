1de69545cc5db3dfe743b3deaf8b1cac46a80739
FrenzyBoy <luohc2004@163.com>
2017-08-21 23:42:50 +0800

    批量加了条目，试验了条目单击功能

--- WebVideoServer/wwwroot/app/viewmodels/welcome.js
+++ WebVideoServer/wwwroot/app/viewmodels/welcome.js
@@ -1,22 +1,31 @@
-﻿define(['durandal/app','knockout'], function (app,ko) {
+﻿define(['durandal/app', 'knockout'], function (app, ko) {
     var ctor = function () {
-        this.displayName = 'Welcome to the Durandal Starter Kit!';
-        this.description = 'Durandal is a cross-device, cross-platform client framework written in JavaScript and designed to make Single Page Applications (SPAs) easy to create and maintain.';
+        this.displayName = '天天学舞蹈的视频';
 
         this.lang = {
             columns: {  // i18n key value set
                 _id: 'ID',
-                title: 'Title',
-                desc: 'Description',
-                date: 'Start Date'
+                title: '名称',
+                uploadDate: '上传时间',
+                captureDate: '拍摄时间'
             }
         };
 
         this.results = ko.observableArray([
-            { _id: 1, title: 'A title', desc: 'A description', date: new Date() },
-            { _id: 1, title: 'B title', desc: 'b description', date: new Date() }
-        ]);
-    }
+            {
+                _id: 1, title: '视频1', uploadDate: (new Date()).toLocaleDateString(), captureDate: (new Date()).toLocaleDateString(), onClick: function () {
+                    alert(this.title);
 
+                }
+            },
+            { _id: 2, title: '视频2', uploadDate: (new Date()).toLocaleDateString(), captureDate: (new Date()).toLocaleDateString() },
+        ]);
+
+        for (var i = 3; i < 23; i++) {
+            this.results.push({
+                _id: i, title: '视频' + i, uploadDate: (new Date()).toLocaleDateString(), captureDate: (new Date()).toLocaleDateString()
+            });
+        }
+    }
     return ctor;
 });
--- WebVideoServer/wwwroot/app/widgets/datatable/viewmodel.js
+++ WebVideoServer/wwwroot/app/widgets/datatable/viewmodel.js
@@ -1,99 +1,108 @@
-﻿define(['durandal/composition', 'knockout', 'jquery'], function(composition, ko, $){
-	
-	'use strict';
+﻿define(['durandal/composition', 'knockout', 'jquery'], function (composition, ko, $) {
 
-	function DataTable(){
-		this.sortColumn = ko.observable(0);
-		this.sortDirection = ko.observable(-1);
-	}
-	
-	DataTable.prototype.activate = function(settings){
-		this.columns = ko.observableArray();
-		this.columnsKeys = ko.observableArray();
-		this.rows = ko.observableArray();
+    'use strict';
 
-		var me = this;
-		this.settings = this._normalizeSettings(settings);
+    function DataTable() {
+        this.sortColumn = ko.observable(0);
+        this.sortDirection = ko.observable(-1);
+    }
 
-		if(ko.isObservable(this.settings.rows)){
-			this.settings.rows.subscribe(function(newValue){
-				me.rows(newValue);
-				me.onRowsChange();
-			});
-			this.rows(this.settings.rows());
-		}else{
-			this.rows(this.settings.rows);
-		}
-		this.onRowsChange();
-	};
+    DataTable.prototype.activate = function (settings) {
+        this.columns = ko.observableArray();
+        this.columnsKeys = ko.observableArray();
+        this.rows = ko.observableArray();
 
-	DataTable.prototype._normalizeSettings = function(settings){
-		var defaults = {
-			rows: [],
-			lang: {},
-			itemPerPage: 0,
-			search: false,
-			columnsBlackList: []
-		};
-		return $.extend({}, defaults, settings);
-	};
+        var me = this;
+        this.settings = this._normalizeSettings(settings);
 
-	DataTable.prototype.onRowsChange = function(){
-		var item = this.settings.rows()[0],
-			key
-		;
-		this.columns([]);
-		this.columnsKeys([]);
-		for(key in item){
-			if(item.hasOwnProperty(key)){
-				if(this.settings.columnsBlackList.indexOf(key) === -1){
-					this.columns.push(this.settings.lang[key]);
-					this.columnsKeys.push(key);
-				}
-			}
-		}
-		
-		this.sort();
-		
-		console.log('columns', this.columns());
-		console.log('columnsKeys', this.columnsKeys());
-	};
-	
-	DataTable.prototype.toggleSort = function(keyIx){
-		this.sortColumn(keyIx);
+        if (ko.isObservable(this.settings.rows)) {
+            this.settings.rows.subscribe(function (newValue) {
+                me.rows(newValue);
+                me.onRowsChange();
+            });
+            this.rows(this.settings.rows());
+        } else {
+            this.rows(this.settings.rows);
+        }
+        this.onRowsChange();
+    };
 
-		if(this.sortDirection() <= 0){
-			this.sortDirection(1);
-		}else{
-			this.sortDirection(-1);
-		}
+    DataTable.prototype._normalizeSettings = function (settings) {
+        var defaults = {
+            rows: [],
+            lang: {},
+            itemPerPage: 0,
+            search: false,
+            columnsBlackList: []
+        };
+        return $.extend({}, defaults, settings);
+    };
 
-		this.sort();
-	};
-	
-	DataTable.prototype.sort = function(){
-		var key = this.columnsKeys()[this.sortColumn()],
-			me = this;
-		this.rows.sort(function(a, b){
-			if(a[key] > b[key]){
-				return -1 * me.sortDirection();
-			}else if(a[key] < b[key]){
-				return 1 * me.sortDirection();
-			}else{
-				return 0;
-			}
-		});
-	};
-	
-	DataTable.prototype._ = function(key, value){
-		key = 'v_' + key;
-		console.log('LANGING', key, value);
-		if(this.settings.lang.hasOwnProperty(key) && this.settings.lang[key].hasOwnProperty(value)){
-			return this.settings.lang[key][value];
-		}else{
-			return value;
-		}
-	};
-		
-	return DataTable;
+    DataTable.prototype.onRowsChange = function () {
+        var item = this.settings.rows()[0],
+            key
+            ;
+        this.columns([]);
+        this.columnsKeys([]);
+        for (key in item) {
+            if (item.hasOwnProperty(key)) {
+                if (this.settings.columnsBlackList.indexOf(key) === -1) {
+                    this.columns.push(this.settings.lang[key]);
+                    this.columnsKeys.push(key);
+                }
+            }
+        }
+
+        this.sort();
+
+        console.log('columns', this.columns());
+        console.log('columnsKeys', this.columnsKeys());
+    };
+
+    DataTable.prototype.toggleSort = function (keyIx) {
+        this.sortColumn(keyIx);
+
+        if (this.sortDirection() <= 0) {
+            this.sortDirection(1);
+        } else {
+            this.sortDirection(-1);
+        }
+
+        this.sort();
+    };
+
+    DataTable.prototype.sort = function () {
+        var key = this.columnsKeys()[this.sortColumn()],
+            me = this;
+        this.rows.sort(function (a, b) {
+            if (a[key] > b[key]) {
+                return -1 * me.sortDirection();
+            } else if (a[key] < b[key]) {
+                return 1 * me.sortDirection();
+            } else {
+                return 0;
+            }
+        });
+    };
+
+    DataTable.prototype._ = function (key, value) {
+        key = 'v_' + key;
+        console.log('LANGING', key, value);
+        if (this.settings.lang.hasOwnProperty(key) && this.settings.lang[key].hasOwnProperty(value)) {
+            return this.settings.lang[key][value];
+        } else {
+            return value;
+        }
+    };
+
+    DataTable.prototype.clickRow = function (row) {
+        if (row.onClick) {
+            console.log('row click', row.onClick);
+            row.onClick();
+        } else {
+            console.log('row not need to response click');
+        }
+    }
+
+    return DataTable;
 });
